a9d589676ade69176c288ef6e3a1b62d836607fb
Syed Muhammad Shahid <155793950+sherazi7862@users.noreply.github.com>
2026-07-14 21:32:47 +0500

    set envernoment varable in main
    Replaced email notification with SMS notification for birthdays and added weather check.

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
 import os
+from twilio.rest import Client
+url = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+weather_parameter ={
+    "lat":18.988850,
+    "lon":73.083138,
+    "appid":my_api,
+    "cnt":4,
+}
+data =requests.get(url,params=weather_parameter)
+weather_data = data.json()
+will_rain = False
+for hour_data in weather_data["list"]:
+    weather_code = hour_data["weather"][0]["id"]
+    if weather_code <700:
+        will_rain =True
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+if will_rain:
+    client = Client(account_sid, auth_token)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+    message = client.messages.create(
+        body="Bring umbrella.",
+        from_="+14246221138",
+        to="+923347178457",
+    )
+print(message.status)
